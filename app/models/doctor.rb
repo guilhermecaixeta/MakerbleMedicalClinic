b06fc8fd02dbc589a7946553f7bf7e6fb31a3d70
Guilherme Lúcio <guilhermecaixeta@users.noreply.github.com>
2024-05-03 16:31:38 +0100

    merge dev to main (#7)
    * Feat/addin-doctors-and-attendants-actions (#1)
    
    * adding screns and new features
    
    * Adding support to charts and rename classes
    
    * completing new features
    
    * fixing devise and update views
    
    * Updating readme and removing not used file
    
    * adding missing validations
    
    * adding missing validations
    
    * fix validation
    
    * fix manager view
    
    * remove duplicated line on merge
    
    * fix bugs on model (#4)
    
    * fixing statistics (#6)

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor < User
-  belongs_to :specialty
+  belongs_to :specialty, required: true
   has_many :appointments, class_name: "Appointment", foreign_key: "user_id", dependent: :delete_all
 
   scope :filter_by_specialty, ->(specialty_id) { select(:id, :name).where(specialty_id: specialty_id) }
